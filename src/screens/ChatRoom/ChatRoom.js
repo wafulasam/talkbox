cf29178e1599d9f28ecc92308ec3d0e9a549503c
Sammie <sammie@Sammies-MacBook-Pro.local>
2021-08-08 20:58:46 +0300

    [feature]: add chat bubble

--- src/screens/ChatRoom/ChatRoom.js
+++ src/screens/ChatRoom/ChatRoom.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './ChatRoom.scss'
+import ChatBubble from 'react-chat-bubble';
+
+const messages = [
+    {
+        "type" : 1,
+        "image": "https://assets.vogue.com/photos/5891552b23f9887c0e0e060d/master/pass/01-holding-yara-shahidi.jpg",
+        "text": "Hello! Good Morning Sam!"
+    }, 
+    {
+        "type": 0,
+        "image": "https://avatars.githubusercontent.com/u/20152051?v=4",
+        "text": "Hi Yara, Good morning!"
+    }
+];
 
 export default function ChatRoom () {
     return (
         <div className="chat-room">
-            <h1>chat room</h1>
+            <ChatBubble messages={messages}/>
         </div>
     )
 }
