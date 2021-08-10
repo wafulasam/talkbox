56698f05a58fbc3c645369e7d3faaa22b2aa30ee
Sammie <sammie@Sammies-MacBook-Pro.local>
2021-08-10 12:40:47 +0300

    [chore]: modify chats

--- src/messages.js
+++ src/messages.js
@@ -3,62 +3,56 @@ export const chats = [
     {
         "type" : 1,
         "image": "https://www.stylevore.com/wp-content/uploads/2019/04/avatar-155445160702l46-150x150.jpg",
-        "text": "Hello! Good Morning Sam!"
+        "text": "Hello! Good Morning Sam"
     }, 
     {
         "type": 0,
         "image": "https://avatars.githubusercontent.com/u/20152051?v=4",
-        "text": "Hi Yara, Good morning!"
+        "text": "Hi Yara, Good morning"
     }
 ]
 
 export const messages = [
         {
-            "type" : 1,
             "image": "https://www.stylevore.com/wp-content/uploads/2019/04/avatar-155445160702l46-150x150.jpg",
-            "text": "Hello! Good Morning Sam!",
+            "text": "Hello! Good Morning Sam",
             "time": "05:20",
             "name": "Yara",
             "id":1
         }, 
         {
-            "type": 0,
-            "image": "https://avatars.githubusercontent.com/u/20152051?v=4",
-            "text": "Hi Yara, Good morning!",
+            "image": "https://i1.sndcdn.com/avatars-000481956306-zhana9-t500x500.jpg",
+            "text": "Good day Boo",
             "time": "08:03",
-            "name": "Sam",
+            "name": "Nicki Minaj",
             "id":2
         },
         {
-            "type" : 1,
-            "image": "https://www.stylevore.com/wp-content/uploads/2019/04/avatar-155445160702l46-150x150.jpg",
-            "text": "Hello! Good Evening Sam!",
+            "image": "https://cdn.allfamous.org/people/avatars/chris-brown-3tml-allfamous.org.jpg?v=56",
+            "text": "Let's meet in Atlanta yo idiot",
             "time": "18:12",
-            "name": "Yara",
+            "name": "Chris Brown",
             "id":3
         }, 
         {
-            "type": 0,
-            "image": "https://avatars.githubusercontent.com/u/20152051?v=4",
-            "text": "Hi Yara, Good evening!",
+            "image": "https://www.stylevore.com/wp-content/uploads/2019/12/avatar-1575980430024pn.jpg",
+            "text": "What! we just became billionaires girl",
             "time": "18:23",
-            "name": "Sam",
+            "name": "Rihanna",
             "id":4
         },
         {
-            "type" : 1,
-            "image": "https://www.stylevore.com/wp-content/uploads/2019/04/avatar-155445160702l46-150x150.jpg",
-            "text": "Hello! Good Morning Sam!",
-            "time": "05:20",
-            "name": "Yara",
-            "id":5
-        }, 
-        {
-            "type": 0,
-            "image": "https://avatars.githubusercontent.com/u/20152051?v=4",
-            "text": "Hi Yara, Good morning!",
+            "image": "https://avatarfiles.alphacoders.com/165/165085.jpg",
+            "text": "I love your latest album",
             "time": "05:23",
-            "name": "Sam",
+            "name": "Ariana",
             "id":6
+        },
+        {
+            "image": "https://avatars.githubusercontent.com/u/39279758?v=4",
+            "text": "The Fluent Team made us proud",
+            "time": "05:20",
+            "name": "CEO Satya",
+            "id":5
         }
 ];
